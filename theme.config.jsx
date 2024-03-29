ced805eb8ed379aa5fa4afba115796ba36caa934
G. A. 02 <blessedbyhope05@gmail.com>
2024-03-29 12:20:01 +0700

    commit

---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>The Continuity of Splines - V2 - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="The Continuity of Splines - V2" />
+          <meta property="og:description" content="In this comprehensive video, Freya Holmér takes us on a deep dive into the world of splines, exploring their continuity, mathematical foundations, and practical applications. From Bézier curves to Hermite and B-splines, she unveils the intricacies of these essential tools used in computer graphics, game development, and industrial design. Through engaging explanations and visual demonstrations, Freya illuminates the concepts of parametric and geometric continuity, showcasing how different spline types achieve smoothness and control. This in-depth exploration is a must-watch for anyone interested in the mathematics behind curves and their impact on digital art and design." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
